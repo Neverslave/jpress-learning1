132256e362c0a504a3df21a8bba837a7eceaa2b6
Zhu <1029110566@qq.com>
2018-11-01 09:39:20 +0800

    Initial commit

---
+++ module-article/module-article-web/src/main/java/io/jpress/module/article/controller/api/ArticleApiController.java
@@ -0,0 +1,273 @@
+/**
+ * Copyright (c) 2016-2019, Michael Yang 杨福海 (fuhai999@gmail.com).
+ * <p>
+ * Licensed under the GNU Lesser General Public License (LGPL) ,Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.jpress.module.article.controller.api;
+
+import com.jfinal.kit.Ret;
+import com.jfinal.plugin.activerecord.Page;
+import io.jboot.db.model.Columns;
+import io.jboot.utils.StrUtils;
+import io.jboot.web.controller.annotation.RequestMapping;
+import io.jpress.model.User;
+import io.jpress.module.article.kit.ArticleKit;
+import io.jpress.module.article.model.Article;
+import io.jpress.module.article.model.ArticleCategory;
+import io.jpress.module.article.model.ArticleComment;
+import io.jpress.module.article.service.ArticleCategoryService;
+import io.jpress.module.article.service.ArticleCommentService;
+import io.jpress.module.article.service.ArticleService;
+import io.jpress.service.OptionService;
+import io.jpress.web.base.ApiControllerBase;
+import org.apache.commons.lang.StringEscapeUtils;
+
+import javax.inject.Inject;
+import java.util.List;
+
+/**
+ * @author Michael Yang 杨福海 （fuhai999@gmail.com）
+ * @version V1.0
+ * @Title: 文章前台页面Controller
+ * @Package io.jpress.module.article.admin
+ */
+@RequestMapping("/api/article")
+public class ArticleApiController extends ApiControllerBase {
+
+    @Inject
+    private ArticleService articleService;
+
+    @Inject
+    private ArticleCategoryService categoryService;
+
+    @Inject
+    private OptionService optionService;
+
+    @Inject
+    private ArticleCommentService commentService;
+
+    /**
+     * 文章详情的api
+     * 可以传 id 获取文章详情，也可以通过 slug 来获取文章详情
+     * 例如：
+     * http://127.0.0.1:8080/api/article?id=123
+     * 或者
+     * http://127.0.0.1:8080/api/article?slug=myslug
+     */
+    public void index() {
+        Long id = getParaToLong("id");
+        if (id != null) {
+            Article article = articleService.findById(id);
+            renderJson(Ret.ok("article", article));
+            return;
+        }
+
+        String slug = getPara("slug");
+        if (slug != null) {
+            Article article = articleService.findFirstBySlug(slug);
+            renderJson(Ret.ok("article", article));
+            return;
+        }
+
+        renderFailJson();
+    }
+
+
+    /**
+     * 获取分类详情的API
+     * <p>
+     * 可以通过 id 获取文章分类，也可以通过 type + slug 定位到分类
+     * 分类可能是后台对应的分类，有可以是一个tag（tag也是一种分类）
+     * <p>
+     * 例如：
+     * http://127.0.0.1:8080/api/article/category?id=123
+     * 或者
+     * http://127.0.0.1:8080/api/article/category?type=category&slug=myslug
+     * http://127.0.0.1:8080/api/article/category?type=tag&slug=myslug
+     */
+    public void category() {
+        Long id = getParaToLong("id");
+        if (id != null) {
+            ArticleCategory category = categoryService.findById(id);
+            renderJson(Ret.ok("category", category));
+            return;
+        }
+
+        String slug = getPara("slug");
+        String type = getPara("type");
+
+        if (StrUtils.isBlank(slug)
+                || StrUtils.isBlank(type)) {
+            renderFailJson();
+            return;
+        }
+
+
+        ArticleCategory category = categoryService.findFirstByTypeAndSlug(type, slug);
+        renderJson(Ret.ok("category", category));
+    }
+
+
+    /**
+     * 通过 分类ID 分页读取文章列表
+     */
+    public void paginate() {
+        Long categoryId = getParaToLong("categoryId");
+        if (categoryId == null || categoryId <= 0) {
+            renderFailJson();
+            return;
+        }
+
+        String orderBy = getPara("orderBy");
+        int pageNumber = getParaToInt("page", 1);
+
+        Page<Article> page = articleService.paginateByCategoryIdInNormal(pageNumber, 10, categoryId, orderBy);
+        renderJson(Ret.ok().set("page", page));
+
+    }
+
+
+    /**
+     * 通过 文章属性 获得文章列表
+     */
+    public void list() {
+        String flag = getPara("flag");
+        Boolean hasThumbnail = getParaToBoolean("hasThumbnail");
+        String orderBy = getPara("orderBy", "id desc");
+        int count = getParaToInt("count", 10);
+
+
+        Columns columns = Columns.create("flag", flag);
+        if (hasThumbnail != null) {
+            if (hasThumbnail) {
+                columns.is_not_null("thumbnail");
+            } else {
+                columns.is_null("thumbnail");
+            }
+        }
+
+        List<Article> articles = articleService.findListByColumns(columns, orderBy, count);
+        renderJson(Ret.ok("articles", articles));
+    }
+
+    /**
+     * 某一篇文章的相关文章
+     */
+    public void relevantList() {
+
+        Long id = getParaToLong("id");
+        if (id == null) {
+            renderFailJson();
+        }
+
+        int count = getParaToInt("count", 3);
+
+        List<Article> relevantArticles = articleService.findRelevantListByArticleId(id, Article.STATUS_NORMAL, count);
+        renderOk("articles", relevantArticles);
+    }
+
+
+    public void save() {
+        Article article = getRawObject(Article.class);
+        articleService.saveOrUpdate(article);
+        renderJson(Ret.ok());
+    }
+
+
+    /**
+     * 发布评论
+     */
+    public void postComment() {
+        Long articleId = getParaToLong("articleId");
+        Long pid = getParaToLong("pid");
+        String content = getRawData();
+
+        if (articleId == null || articleId <= 0) {
+            renderJson(Ret.fail());
+            return;
+        }
+
+        if (StrUtils.isBlank(content)) {
+            renderJson(Ret.fail().set("message", "评论内容不能为空"));
+            return;
+        } else {
+            content = StringEscapeUtils.escapeHtml(content);
+        }
+
+
+        Article article = articleService.findById(articleId);
+        if (article == null) {
+            renderJson(Ret.fail());
+            return;
+        }
+
+        // 文章关闭了评论的功能
+        if (!article.isCommentEnable()) {
+            renderJson(Ret.fail().set("message", "该文章的评论功能已关闭"));
+            return;
+        }
+
+        //是否开启评论功能
+        Boolean commentEnable = optionService.findAsBoolByKey("article_comment_enable");
+        if (commentEnable == null || commentEnable == false) {
+            renderJson(Ret.fail().set("message", "评论功能已关闭"));
+            return;
+        }
+
+        User user = getLoginedUser();
+
+        ArticleComment comment = new ArticleComment();
+
+        comment.setArticleId(articleId);
+        comment.setContent(content);
+        comment.setPid(pid);
+        comment.setEmail(user.getEmail());
+
+        comment.setUserId(user.getId());
+        comment.setAuthor(user.getNickname());
+
+        //是否是管理员必须审核
+        Boolean reviewEnable = optionService.findAsBoolByKey("article_comment_review_enable");
+        if (reviewEnable != null && reviewEnable == true) {
+            comment.setStatus(ArticleComment.STATUS_UNAUDITED);
+        }
+        /**
+         * 无需管理员审核、直接发布
+         */
+        else {
+            comment.setStatus(ArticleComment.STATUS_NORMAL);
+        }
+
+        //记录文章的评论量
+        articleService.doIncArticleCommentCount(articleId);
+
+        if (pid != null) {
+            //记录评论的回复数量
+            commentService.doIncCommentReplyCount(pid);
+        }
+        commentService.saveOrUpdate(comment);
+
+        Ret ret = Ret.ok();
+        if (comment.isNormal()) {
+            ret.set("comment", comment).set("code", 0);
+        } else {
+            ret.set("code", 0);
+        }
+
+        renderJson(ret);
+
+        ArticleKit.doNotifyAdministratorByEmail(article, comment);
+    }
+
+
+}
---
+++ module-article/module-article-web/src/main/java/io/jpress/module/article/directive/CategoriesDirective.java
@@ -0,0 +1,113 @@
+/**
+ * Copyright (c) 2016-2019, Michael Yang 杨福海 (fuhai999@gmail.com).
+ * <p>
+ * Licensed under the GNU Lesser General Public License (LGPL) ,Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.jpress.module.article.directive;
+
+import com.jfinal.template.Env;
+import com.jfinal.template.io.Writer;
+import com.jfinal.template.stat.Scope;
+import io.jboot.web.JbootControllerContext;
+import io.jboot.web.directive.annotation.JFinalDirective;
+import io.jboot.web.directive.base.JbootDirectiveBase;
+import io.jpress.JPressConsts;
+import io.jpress.commons.layer.SortKit;
+import io.jpress.module.article.model.Article;
+import io.jpress.module.article.model.ArticleCategory;
+import io.jpress.module.article.service.ArticleCategoryService;
+
+import javax.inject.Inject;
+import java.util.List;
+
+/**
+ * @author Michael Yang 杨福海 （fuhai999@gmail.com）
+ * @version V1.0
+ * @Title: 文章分类：分类、专题、标签等
+ * @Package io.jpress.module.article.directives
+ */
+@JFinalDirective("categories")
+public class CategoriesDirective extends JbootDirectiveBase {
+
+    @Inject
+    private ArticleCategoryService categoryService;
+
+    @Override
+    public void onRender(Env env, Scope scope, Writer writer) {
+
+        String type = getPara("type", scope, ArticleCategory.TYPE_CATEGORY);
+        Boolean asTree = getPara("asTree", scope, Boolean.FALSE);
+
+        List<ArticleCategory> categories = categoryService.findListByType(type);
+        if (categories == null || categories.isEmpty()) {
+            return;
+        }
+
+        doFlagIsActiveByCurrentCategory(categories);
+        doFlagIsActiveByCurrentArticle(categories);
+
+        if (asTree != null && asTree == true) {
+            SortKit.toTree(categories);
+        }
+
+        scope.setLocal("categories", categories);
+        renderBody(env, scope, writer);
+    }
+
+
+    private void doFlagIsActiveByCurrentCategory(List<ArticleCategory> categories) {
+
+        ArticleCategory currentCategory = JbootControllerContext.get().getAttr("category");
+
+        //当前页面并不是某个分类页面
+        if (currentCategory == null) {
+            return;
+        }
+
+        doFlagByCurrentCategory(categories, currentCategory);
+
+    }
+
+
+    private void doFlagIsActiveByCurrentArticle(List<ArticleCategory> categories) {
+        Article currentArticle = JbootControllerContext.get().getAttr("article");
+
+        //当前页面并不是文章详情页面
+        if (currentArticle == null) {
+            return;
+        }
+
+        List<ArticleCategory> articleCategories = categoryService.findActiveCategoryListByArticleId(currentArticle.getId());
+        if (articleCategories == null || articleCategories.isEmpty()) {
+            return;
+        }
+
+        for (ArticleCategory articleCategory : articleCategories) {
+            doFlagByCurrentCategory(categories, articleCategory);
+        }
+    }
+
+
+    private void doFlagByCurrentCategory(List<ArticleCategory> categories, ArticleCategory currentCategory) {
+        for (ArticleCategory category : categories) {
+            if (currentCategory.getId().equals(category.getId())) {
+                JPressConsts.doFlagModelActive(category);
+            }
+        }
+    }
+
+    @Override
+    public boolean hasEnd() {
+        return true;
+    }
+}
